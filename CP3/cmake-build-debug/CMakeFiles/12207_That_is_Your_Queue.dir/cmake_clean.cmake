90734b0ffb21a43932a179d6d698281a6aefd9cb
Phong V. Cao <phongvcao@phongvcao.com>
2020-01-26 19:07:04 -0800

    Add C++ directives

---
+++ CP3/cmake-build-debug/CMakeFiles/12207_That_is_Your_Queue.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "12207_That_is_Your_Queue"
+  "12207_That_is_Your_Queue.pdb"
+  "CMakeFiles/12207_That_is_Your_Queue.dir/2.2/12207_That_is_Your_Queue.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/12207_That_is_Your_Queue.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
